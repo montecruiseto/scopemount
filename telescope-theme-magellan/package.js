e139009391472b924803ad9f0f52fa3d3cf5c179
montecruiseto <samir.bekaert@gmail.com>
2015-01-20 20:29:19 +0700

    Add Magellan base

---
+++ telescope-theme-magellan/package.js
@@ -0,0 +1,20 @@
+Package.describe({summary: "Telescope Magellan theme"});
+
+Package.onUse(function (api) {
+
+  api.use(
+    [
+    ],
+    ['client', 'server']
+  );
+
+  api.addFiles([
+    ], ['client', 'server']);
+
+  api.addFiles(
+    [
+    ],
+    'client'
+  );
+
+});
