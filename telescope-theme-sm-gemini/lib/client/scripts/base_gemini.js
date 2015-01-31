2e301bf860c430188b2340d0bc7a3e895bd9a680
montecruiseto <samir.bekaert@gmail.com>
2015-01-31 20:32:18 +0700

    work in progress

---
+++ telescope-theme-sm-gemini/lib/client/scripts/base_gemini.js
@@ -0,0 +1,50 @@
+postModules = [
+
+  {
+    template: 'postUpvote',
+    order: 10
+  },
+  {
+    template: 'postContent',
+    order: 20
+  }
+
+];
+
+postHeading = [
+
+  {
+    template: 'postTitle',
+    order: 10
+  },
+  {
+    template: 'postAuthor',
+    order: 20
+  },
+  {
+    template: 'postInfo',
+    order: 30
+  },
+  {
+    template: 'postCommentsLink',
+    order: 40
+  },
+  {
+    template: 'postAdmin',
+    order: 50
+  },
+
+];
+
+postMeta = [
+
+  {
+    template: 'postAvatars',
+    order: 50
+  },
+  {
+    template: 'postActions',
+    order: 60
+  }
+
+];
---
+++ telescope-theme-sm-gemini/package.js
@@ -0,0 +1,64 @@
+Package.describe({
+  summary: "Telescope scopemount-gemini theme",
+  version: '0.1.0',
+  name: "telescope-theme-sm-gemini"
+});
+
+Package.onUse(function (api) {
+
+  api.use(
+    [
+      'templating',
+      'telescope-lib',
+      'telescope-base',
+      'telescope-theme-hubble',
+      'fourseven:scss'
+    ],
+    ['client', 'server']
+  );
+
+  api.addFiles([
+    //
+    ], ['client', 'server']);
+
+  api.addFiles(
+    [
+      // globals
+      'lib/client/scss/global/_forms.scss',
+      'lib/client/scss/global/_links.scss',
+
+      // modules
+      'lib/client/scss/modules/_layout.scss',
+      'lib/client/scss/modules/_nav.scss',
+      'lib/client/scss/modules/_mobile_nav.scss',
+      'lib/client/scss/modules/_posts.scss',
+
+      // partials
+      'lib/client/scss/partials/_colors.scss',
+      'lib/client/scss/partials/_grid.scss',
+      'lib/client/scss/partials/_typography.scss',
+
+      // screen
+      'lib/client/scss/screen.scss',
+
+      // overrides
+      'lib/client/templates/nav_mod.html',
+      'lib/client/templates/post_info_mod.html',
+
+      // customizations
+      'lib/client/scripts/base_gemini.js',
+
+      // images
+      'img/header-bg.jpg'
+
+    ],
+    'client'
+  );
+
+  api.export([
+    'postModules',
+    'postHeading',
+    'postMeta'
+  ]);
+
+});
