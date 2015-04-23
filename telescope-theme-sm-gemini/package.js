7bd774c6750fa6e0236a54d59f8856824b16fe9f
montecruiseto <samir.bekaert@gmail.com>
2015-04-23 16:57:24 +0700

    Hotfix error panel

--- telescope-theme-sm-gemini/package.js
+++ telescope-theme-sm-gemini/package.js
@@ -30,6 +30,7 @@ Package.onUse(function (api) {
       // modules
       'lib/client/scss/modules/_avatars.scss',
       'lib/client/scss/modules/_comments.scss',
+      'lib/client/scss/modules/_errors.scss',
       'lib/client/scss/modules/_layout.scss',
       'lib/client/scss/modules/_nav.scss',
       'lib/client/scss/modules/_mobile_nav.scss',
