be391b0451d4f9f410d8d50f994303fd7e8d73c3
montecruiseto <samir.bekaert@gmail.com>
2015-05-27 21:06:57 +0700

    Upgrade cards

--- telescope-theme-sm-palomar/lib/client/scripts/icons_palomar.js
+++ telescope-theme-sm-palomar/lib/client/scripts/icons_palomar.js
@@ -1,7 +1,5 @@
 // override icons
 
-Telescope.utils.icons.upvote = "long-arrow-up";
-Telescope.utils.icons.downvote = "long-arrow-down";
-Telescope.utils.icons.voted = "long-arrow-up";
-Telescope.utils.icons.open = "chevron-down";
-Telescope.utils.icons.close = "chevron-up";
+Telescope.utils.icons.upvote = "star-o";
+Telescope.utils.icons.downvote = "star-half-empty";
+Telescope.utils.icons.voted = "star";
