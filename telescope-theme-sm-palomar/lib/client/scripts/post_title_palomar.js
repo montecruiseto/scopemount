a18d8de1c3ec24b3bcb8ec05ed21c7e6699c6337
montecruiseto <samir.bekaert@gmail.com>
2015-05-29 20:07:35 +0700

    Upgrade mobile views

--- telescope-theme-sm-palomar/lib/client/scripts/post_title_palomar.js
+++ telescope-theme-sm-palomar/lib/client/scripts/post_title_palomar.js
@@ -1,7 +1,15 @@
 Template.post_title.onRendered(function() {
     // Call the Trunk8 jQuery plugin on the post titles
+    console.log('rendered');
     this.$('.post-title').trunk8({
-        lines: 3
+        lines: 2
     });
 });
 
+Meteor.startup(function() {
+    /* Handle browser resize too */
+    $(window).resize(function (e) {
+    	console.log('yep');
+      $('.post-title').trunk8();
+    });
+});
