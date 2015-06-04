9d2cfc9a233c5db6230cdf0c5cf3d65c527baa1f
montecruiseto <samir.bekaert@gmail.com>
2015-06-04 13:21:17 +0700

    Initial commit

---
+++ telescope-theme-sm-sloan/README.md
@@ -0,0 +1,27 @@
+# Telescope Sloan Theme
+
+*Note: Scopemount themes are up-to-date with Telescope v0.20.4.*
+
+Sloan is part of [Scopemount](http://scopemount.startrack.io), a set of free professional themes for [Telescope](http://www.telescopeapp.org/).
+
+[![Imgur](http://i.imgur.com/8yYLXiY.jpg)](http://scopemount.startrack.io)
+
+## Install & Setup
+
+See [Blog Post](http://blog.startrack.io/scopemount-theme-sloan/)
+
+### Look & Feel
+
+Sloan is a modern looking Q&A theme - [Demo](http://sm-sloan.meteor.com/)
+
+![Sloan]()
+
+![Sloan]()
+
+### Thanks
+
+[Sacha Greif](https://github.com/SachaG) for building [Telescope](https://github.com/TelescopeJS/Telescope).
+
+### License
+
+Scopemount is licensed under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ telescope-theme-sm-sloan/package.js
@@ -0,0 +1,62 @@
+Package.describe({
+  name: "montecruiseto:telescope-theme-sm-sloan",
+  summary: "Telescope Scopemount: Sloan theme package",
+  version: "0.1.0",
+  git: "https://github.com/montecruiseto/scopemount.git"
+});
+
+Package.onUse(function (api) {
+
+  api.versionsFrom("METEOR@1.0");
+
+  api.use([
+      // core dependencies
+      'telescope:core@0.20.4',
+      'telescope:theme-base@0.20.4',
+      'telescope:theme-hubble@0.20.4'
+    ]);
+
+  api.addFiles([
+      'lib/modules_sloan.js'
+  ], ['client', 'server']);
+
+  api.addFiles([
+      // globals
+      'lib/client/scss/global/_forms.scss',
+      'lib/client/scss/global/_links.scss',
+      'lib/client/scss/global/_forms.scss',
+      'lib/client/scss/global/_markdown.scss',
+
+      // modules
+      'lib/client/scss/modules/_avatars.scss',
+      'lib/client/scss/modules/_comments.scss',
+      'lib/client/scss/modules/_layout.scss',
+      'lib/client/scss/modules/_menu.scss',
+      'lib/client/scss/modules/_nav.scss',
+      'lib/client/scss/modules/_mobile_nav.scss',
+      'lib/client/scss/modules/_posts.scss',
+      'lib/client/scss/modules/_search.scss',
+      'lib/client/scss/modules/_errors.scss',
+
+      // partials
+      'lib/client/scss/partials/_typography.scss',
+      'lib/client/scss/partials/_grid.scss',
+      'lib/client/scss/partials/_colors.scss',
+
+      // screen
+      'lib/client/scss/screen.scss',
+
+      // overrides
+      'lib/client/templates/post_content_sloan.html',
+      'lib/client/templates/post_domain_sloan.html',
+      'lib/client/templates/post_info_sloan.html',
+      'lib/client/templates/post_upvote_sloan.html',
+      'lib/client/templates/post_page_sloan.html',
+
+      // customizations
+      'lib/client/scripts/icons_sloan.js',
+      'lib/client/scripts/templates_sloan.js'
+
+    ], ['client']);
+
+});
