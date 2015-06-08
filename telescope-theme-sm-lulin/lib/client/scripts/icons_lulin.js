db65b73feacd0258691f575f82859738efc9a5b6
montecruiseto <samir.bekaert@gmail.com>
2015-06-08 21:19:18 +0700

    Revert add lulin base

--- telescope-theme-sm-lulin/lib/client/scripts/icons_lulin.js
+++
@@ -1,5 +0,0 @@
-// override icons
-
-Telescope.utils.icons.upvote = "star-o";
-Telescope.utils.icons.downvote = "star-half-empty";
-Telescope.utils.icons.voted = "star";
--- telescope-theme-sm-lulin/lib/client/scripts/post_title_lulin.js
+++
@@ -1,6 +0,0 @@
-Template.post_title.onRendered(function() {
-    // Call the Succinct plugin on the post titles
-    $('.post-title').succinct({
-           size: 66
-       });
-});
