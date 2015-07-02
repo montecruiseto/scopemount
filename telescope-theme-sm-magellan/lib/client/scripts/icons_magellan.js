7eb4c70b3ddbd563d8ddbf6e07e6e266d57a884d
montecruiseto <samir.bekaert@gmail.com>
2015-07-02 13:11:50 +0700

    Upgrade styles

--- telescope-theme-sm-magellan/lib/client/scripts/icons_magellan.js
+++ telescope-theme-sm-magellan/lib/client/scripts/icons_magellan.js
@@ -1,5 +1,5 @@
 // override icons
 
-Telescope.utils.icons.upvote = "heart-o";
-Telescope.utils.icons.voted = "heart";
-Telescope.utils.icons.comment = "th-list";
+Telescope.utils.icons.upvote = "chevron-up";
+Telescope.utils.icons.voted = "chevron-up";
+Telescope.utils.icons.comment = "comments";
