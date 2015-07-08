e37b01f2b60414c016c4b1204473e40d9541d8fd
montecruiseto <samir.bekaert@gmail.com>
2015-07-08 09:05:58 +0700

    Upgrade styles

--- telescope-theme-sm-laval/lib/client/scripts/post_item_laval.js
+++ telescope-theme-sm-laval/lib/client/scripts/post_item_laval.js
@@ -1,34 +1,7 @@
 Template.post_item.onRendered(function() {
     // Set a random color for each new post item
-      // var swatches = document.querySelectorAll(".laval-post");
-      // console.log(swatches);
-      // for (var i in swatches) {
-      //     var swatch = swatches[i];
-      //     var color = randomColor({
-      //         luminosity: 'bright',
-      //         hue: 'blue'
-      //     });
-      //     if (swatch.style) {
-      //         swatch.style.background = color;
-      //     }
-      // }
-      // var color = randomColor({
-      //     luminosity: 'bright',
-      //     hue: 'blue'
-      // });
-      var swatch = $(this).find('.laval-post');
-      console.log(swatch);
-
-      // for (var i in swatches) {
-      //     var swatch = swatches[i];
-
-      //     if (swatch.style) {
-      //         swatch.style.background = color;
-      //     }
-      // }
-
-      // var renderedPosts = document.querySelectorAll(".laval-post");
-      // console.log(renderedPosts);
-      // renderedPosts.className = ".laval-post-rendered";
-      // document.querySelectorAll(".laval-post").className = ".laval-post-rendered";
+    $(this.find('.laval-post')).css('background', randomColor({
+        luminosity: 'bright',
+        hue: 'blue'
+    }));
 });
