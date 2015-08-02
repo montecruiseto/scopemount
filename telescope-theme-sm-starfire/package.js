74d97f0735c176745aeb3637e346145db72cb6dd
montecruiseto <samir.bekaert@gmail.com>
2015-08-02 07:59:22 +0700

    Changes

--- telescope-theme-sm-starfire/package.js
+++ telescope-theme-sm-starfire/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "montecruiseto:telescope-theme-sm-starfire",
   summary: "Telescope Scopemount: Starfire theme package",
-  version: "0.1.5",
+  version: "0.1.6",
   git: "https://github.com/montecruiseto/scopemount.git"
 });
 
