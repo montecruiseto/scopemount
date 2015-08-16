c812ad5903e70feb281f0b38b40218db793926de
montecruiseto <samir.bekaert@gmail.com>
2015-08-16 14:48:36 +0700

    Fix logo colour

--- telescope-theme-sm-laval/package.js
+++ telescope-theme-sm-laval/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "montecruiseto:telescope-theme-sm-laval",
   summary: "Telescope Scopemount: Laval theme package",
-  version: "0.1.3",
+  version: "0.1.5",
   git: "https://github.com/montecruiseto/scopemount.git"
 });
 
